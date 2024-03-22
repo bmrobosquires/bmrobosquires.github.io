f62d2ff5ab980c2eed37bd58f00a991fb5d6b251
bmrobosquires <144856537+bmrobosquires@users.noreply.github.com>
2024-03-22 15:37:25 -0400

    Update crescendo_config.js
    Reverted to original to hopefully fix bug

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023FIM1",
+      "defaultValue": "2023tnkn",
       "required": "true"
     },
     { "name": "Match Level",
@@ -54,6 +54,14 @@ var config_data = `
       "type": "team",
       "min": 1,
       "max": 99999
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png",
+      "clickRestriction": "one",
+      "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
+      "shape": "circle 5 black red true"
     }
   ],
   "auton": [
@@ -79,6 +87,10 @@ var config_data = `
       "code": "tss",
       "type": "counter"
     },
+    { "name": "Times Amplified",
+      "code": "tta",
+      "type": "counter"
+    },
     { "name": "Pickup From",
       "code": "tpu",
       "type": "radio",
@@ -92,6 +104,10 @@ var config_data = `
     }
   ],
   "endgame": [
+    { "name": "Stage Timer",
+      "code": "dt",
+      "type": "timer"
+    },
     { "name": "Final Status",
       "code": "fs",
       "type":"radio",
@@ -100,7 +116,6 @@ var config_data = `
         "o": "Onstage<br>",
         "s": "Onstage (Spotlit)<br>",
         "h": "Harmony<br>",
-        "t": "Harmony & spotlit<br>",
         "a": "Attempted but failed<br>",
         "x": "Not attempted"
       },
