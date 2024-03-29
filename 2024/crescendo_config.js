ed52b89b5799bf46767b7c6e967d605d8eccd755
bmrobosquires <144856537+bmrobosquires@users.noreply.github.com>
2024-03-29 15:43:30 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024FIM1",
+      "defaultValue": "2024miken",
       "required": "true"
     },
     { "name": "Match #",
