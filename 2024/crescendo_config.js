e85143772f804bfdc176de431a75851db43bd5dc
bmrobosquires <144856537+bmrobosquires@users.noreply.github.com>
2024-10-26 08:30:57 -0400

    Update crescendo_config.js
    changed event

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024micmp1",
+      "defaultValue": "2024MIZee",
       "required": "true"
     },
     { "name": "Match #",
